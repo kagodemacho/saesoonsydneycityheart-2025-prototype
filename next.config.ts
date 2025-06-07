eea6993f7b9d4edf4af2957c90eebfc09bb7a2e1
KagoDeMacho <kagodemacho@gmail.com>
2025-06-07 12:17:42 +1000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,7 @@ const isProd = process.env.NODE_ENV == 'production';
 const nextConfig: NextConfig = {
   basePath: isProd ? '/saesoonsydneycityheart-2025-prototype':'',
   output: 'export',
-  distDir: 'dist',
-  images: {
-    unoptimized:true
-  }
+  distDir: 'dist'
 };
 
 export default nextConfig;
