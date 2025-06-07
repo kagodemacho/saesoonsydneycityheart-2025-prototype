4afed7603eb0fedc3149c37975dc3e9325ae7061
KagoDeMacho <kagodemacho@gmail.com>
2025-06-07 12:34:15 +1000

    build->dist

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,12 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV == 'production';
 
 const nextConfig: NextConfig = {
-  // basePath: isProd ? '/saesoonsydneycityheart-2025-prototype':'',
-  // output: 'export',
-  // distDir: 'dist',
-  // images: {
-  //   unoptimized:true
-  // }
+  basePath: isProd ? '/saesoonsydneycityheart-2025-prototype':'',
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized:true
+  }
 };
 
 export default nextConfig;
